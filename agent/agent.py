0eb77ef078e430a1631ca9d1f8da33d9c3fbcf72
echen4628 <echen4628@gmail.com>
2025-05-02 17:54:32 -0700

    basic agent with memory

---
+++ agent/agent.py
@@ -0,0 +1,48 @@
+from typing import Annotated
+
+from typing_extensions import TypedDict
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import MemorySaver
+
+
+from langchain_openai import ChatOpenAI
+from utils.constants import GPT_4o
+
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+memory = MemorySaver()
+llm = ChatOpenAI(model=GPT_4o)
+
+
+def chatbot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
+
+
+graph_builder = StateGraph(State)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge("chatbot", END)
+graph = graph_builder.compile(checkpointer=memory)
+
+if __name__ == "__main__":
+    config = {"configurable": {"thread_id": "1"}}
+
+    def stream_graph_updates(user_input: str):
+        for events in graph.stream({"messages": [{"role": "user", "content": user_input}]},
+                                config,
+                                stream_mode="updates"):
+            # import pdb; pdb.set_trace()
+            for value in events.values():
+                print("Assistant:", value["messages"][-1].content)
+
+
+    while True:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+        stream_graph_updates(user_input)
